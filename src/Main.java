2451e78177c3fa2b643a81ffbdeed65373d03529
devopsengineering06 <119695498+devopsengineering06@users.noreply.github.com>
2022-12-04 13:56:32 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Jenkins!");
+        System.out.println("Github değişiklik yapıldı.Hello Jenkins!");
     }
 }
